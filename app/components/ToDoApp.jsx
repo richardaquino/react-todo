59a77adf2c2cda9e0b2bb8b6e2b73db891d695e4
Richard Aquino <richard.aquino@archibus-phil.com>
2020-04-21 17:23:05 +0800

    Add static to do list

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,12 +1,38 @@
 var React = require('react');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+    getInitialState: function() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the Dog'
+                }, 
+                {
+                    id: 2,
+                    text: 'Clean the yard'
+                }, 
+                {
+                    id: 3,
+                    text: 'Wash the Dishes'
+                },
+                , 
+                {
+                    id: 4,
+                    text: 'Create an Application'
+                }
+            ]
+        };
+    },
     render: function() {
+        var {todos} = this.state;
+
         return (
             <div>
-                ToDoApp.jsx
+                <ToDoList todos={todos} />
             </div>
-        )
+        ) 
     }
 
 });
