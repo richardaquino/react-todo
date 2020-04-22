12d0c1e0f03e6de54be3a4407cab9647ce547ffa
Richard Aquino <richard.aquino@archibus-phil.com>
2020-04-22 13:30:42 +0800

    Add Search component

---
+++ app/components/Search.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+
+var Search = React.createClass ({
+    handleSearch: function() {
+        var showCompleted = this.refs.showCompleted.checked;
+        var searchText = this.refs.searchText.value;
+
+        
+        this.props.onSearch(showCompleted, searchText);
+
+    },
+    render: function() {
+        return (
+            <div>
+                <div>
+                    <input type="search" ref="searchText" placeholder="Search" onChange={this.handleSearch}/>
+                </div>
+                <div>
+                    <label>
+                        <input type="checkbox" ref="showCompleted" onChange={this.handleSearch} />
+                        Show Completed To Dos
+                    </label>
+                </div>
+                
+             </div>
+
+
+        )
+    }
+});
+
+module.exports = Search;
--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
 var ToDoList = require('ToDoList');
 var AddToDo = require('AddToDo');
+var Search = require('Search');
 
 var ToDoApp = React.createClass({
     getInitialState: function() {
         return {
+            showCompleted: false,
+            searchText: '',
             todos: [
                 {
                     id: 1,
@@ -29,11 +32,18 @@ var ToDoApp = React.createClass({
     handleAddToDo: function(text) {
         alert('New To Do: ' + text);
     }, 
+    handleSearch: function(showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        });
+    },
     render: function() {
         var {todos} = this.state;
 
         return (
             <div>
+                <Search onSearch={this.handleSearch} />
                 <ToDoList todos={todos} />
                 <AddToDo onAddToDo={this.handleAddToDo} />
             </div>
